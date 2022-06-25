12f9512393e949db7f88e732559859b596bb6701
Basudev Bhowmik <61342612+assamjobz@users.noreply.github.com>
2022-06-25 20:25:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
       <Head>
         <title>Best Backlicks for the Assam Websites, Assam Govt Website, Assamese Backlink</title>
         <link rel="icon" href="/favicon.ico" />
